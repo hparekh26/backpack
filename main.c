523cc21f5c2a1e9a78aec14374f3b98fb2f62f3d
Coding Rooms <git@codingrooms.io>
2024-10-15 15:35:43 +0000

    Declared maximum value

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // The maximum value obtained from the warm-up activity is $650
+    int max_value = 650;
+
+    // Print the message with the maximum value
+    printf("The maximum value is %d\n", max_value);
+
+    return 0;
+}
